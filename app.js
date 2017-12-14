b452366257ca77c6107248030d3ea953691f408c
Daniel Jordan Osborn <djorborn@gmail.com>
2017-12-14 12:16:11 -0500

    Working

--- app.js
+++ app.js
@@ -1,10 +1,4 @@
-var express = require('express');
-var app = express();
-
-app.use([express.urlencoded({extended: true}), express.json(), function(req, res, next){
-  if(req.method == '/test'){
-    res.send("Test");
-  } else {
-    next();
-  }
-}]).listen(3000);
+require('express')()
+.use(function(req, res){
+  res.send("Hello Express")})
+.listen(3000);
